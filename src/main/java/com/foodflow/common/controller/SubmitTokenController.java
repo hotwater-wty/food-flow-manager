a6857012c1ea89c330674150bd539a133f94e424
hotwater-wty <2622206360@qq.com>
2026-07-02 23:11:32 +0800

    fix: 扩大swagger-ui的扫描范围，防重复提交功能测试通过

--- src/main/java/com/foodflow/common/controller/SubmitTokenController.java
+++ src/main/java/com/foodflow/common/controller/SubmitTokenController.java
@@ -1,7 +1,6 @@
 package com.foodflow.common.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foodflow.common.context.LoginContext;
